SVFI 3.9.3 - Fix Color banding in low precision mode with HDR input

RIFE/inference_rife.py
@@ -114,10 +114,7 @@ class RifeInterpolation(VideoFrameInterpolationBase):
         else:
             mid = self.model.inference(i1, i2, scale, iter_time=self.tta_iter)
         del i1, i2
-        if RGB_TYPE.DTYPE == np.uint16:
-            mid = ((mid[0] * RGB_TYPE.SIZE).int().cpu().numpy().transpose(1, 2, 0))[:h, :w].copy()
-        else:
-            mid = ((mid[0] * RGB_TYPE.SIZE).byte().cpu().numpy().transpose(1, 2, 0))[:h, :w].copy()
+        mid = ((mid[0] * RGB_TYPE.SIZE).float().cpu().numpy().transpose(1, 2, 0))[:h, :w].copy()
         return mid
 
     def _make_n_inference(self, img1, img2, scale, n):
@@ -257,10 +254,7 @@ class RifeMultiInterpolation(RifeInterpolation):
         else:
             mids = self.model.inference(i1, i2, scale, n)
         del i1, i2
-        if RGB_TYPE.DTYPE == np.uint16:
-            mids = [((mid[0] * RGB_TYPE.SIZE).int().cpu().numpy().transpose(1, 2, 0))[:h, :w] for mid in mids]
-        else:
-            mids = [((mid[0] * RGB_TYPE.SIZE).byte().cpu().numpy().transpose(1, 2, 0))[:h, :w] for mid in mids]
+        mids = [((mid[0] * RGB_TYPE.SIZE).float().cpu().numpy().transpose(1, 2, 0))[:h, :w] for mid in mids]
         return mids
 
     def _make_n_inference(self, img1, img2, scale, n):

one_line_shot_args.py
@@ -533,7 +533,7 @@ class ReadFlow(IOFlow):
             scale_args = f",scale=in_color_matrix={output_dict['-colorspace']}:out_color_matrix={output_dict['-colorspace']}"
         if not frame_check and not self.ARGS.is_quick_extract:
             scale_args = f",format=yuv444p10le{scale_args},format=rgb48be"
-            if RGB_TYPE.DTYPE == np.uint8:  # 24
+            if RGB_TYPE.DTYPE == np.uint8 and self.ARGS.hdr_mode == HDR_STATE.NONE:  # No format filter for hdr in 8bit
                 scale_args += ",format=rgb24"
             output_dict.update({"-sws_flags": "+bicubic+full_chroma_int+accurate_rnd", })
         vf_args += f"{scale_args},minterpolate=fps={self.ARGS.target_fps:.3f}:mi_mode=dup"