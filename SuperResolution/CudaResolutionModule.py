SVFI 3.10.4     Update Log     - Fix skvideo not find proc attribute error     - Fix Cuda SR not supported full precision(float32)     - Add Batch Mode for Render and Extract Method     - Fix Scene Blend resulting in failed img

SuperResolution/CudaResolutionModule.py
@@ -46,7 +46,7 @@ class CudaSuperResolutionBase:
 
     @torch.no_grad()
     def process(self):
-        self.img = torch.from_numpy(self.img).to(self.device)
+        self.img = torch.from_numpy(self.img).to(self.device).float()
         if self.half:
             self.img = self.img.half()
         self.output = self.model(self.img)
@@ -92,7 +92,7 @@ class CudaSuperResolutionBase:
 
                 # upscale tile
                 with torch.no_grad():
-                    input_tile = torch.from_numpy(input_tile).to(self.device)
+                    input_tile = torch.from_numpy(input_tile).to(self.device).float()
                     if self.half:
                         input_tile = input_tile.half()
                     output_tile = self.model(input_tile)

Utils/utils.py
@@ -78,19 +78,19 @@ class ArgumentManager:
     is_free = False
     is_release = False
     traceback_limit = 0 if is_release else None
-    gui_version = "3.10.3"
+    gui_version = "3.10.4"
     version_tag = f"{gui_version}-alpha " \
                   f"{'Professional' if not is_free else 'Community'} - {'Steam' if is_steam else 'Retail'}"
-    ols_version = "7.4.9"
+    ols_version = "7.4.10"
     """ 发布前改动以上参数即可 """
 
     update_log = f"""
     {version_tag}
     Update Log
-    - Add BT709 - BT2020 HDR10 Function
-    - Optimize High Precision Workflow Efficiency by normalizing input to 16bit at skvideo module
-    - Optimize Cuda SR Module VRAM Occupation
-    - Fix Apply Presets Not covering all options
+    - Fix skvideo not find proc attribute error
+    - Fix Cuda SR not supported full precision(float32)
+    - Add Batch Mode for Render and Extract Method
+    - Fix Scene Blend resulting in failed img
     """
 
     path_len_limit = 230
@@ -466,7 +466,13 @@ class Tools:
         step = 1 / n
         beta = 0
         output = list()
+        def normalize_img(img):
+            if img.dtype in (np.uint16, np.dtype('>u2'), np.dtype('<u2')):
+                img = img.astype(np.uint16)
+            return img
 
+        img0 = normalize_img(img0)
+        img1 = normalize_img(img1)
         for _ in range(n - 1):
             beta += step
             alpha = 1 - beta
@@ -1336,12 +1342,12 @@ class TransitionDetection_ST:
         if not self.scdet_output:
             return
         try:
-            comp_stack = np.hstack((self.img1, self.img2)).astype(np.uint8)
+            comp_stack = np.hstack((self.img1, self.img2))
             comp_stack = cv2.resize(comp_stack, (960, int(960 * comp_stack.shape[0] / comp_stack.shape[1])),
                                     interpolation=cv2.INTER_AREA)
             cv2.putText(comp_stack,
                         title,
-                        (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 0, 0))
+                        (30, 30), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (int(RGB_TYPE.SIZE), 0, 0))
             if "pure" in title.lower():
                 path = f"{self.scdet_cnt:08d}_pure.png"
             elif "band" in title.lower():

skvideo/io/abstract.py
@@ -54,6 +54,7 @@ class VideoReaderAbstract(object):
 
         """
         # check if FFMPEG exists in the path
+        self._proc = None
         assert _HAS_FFMPEG, "Cannot find installation of real FFmpeg (which comes with ffprobe)."
 
         self._filename = filename
@@ -245,7 +246,6 @@ class VideoReaderAbstract(object):
             self._proc.stdout.close()
             self._proc.stderr.close()
             self._terminate(0.2)
-        self._proc = None
 
     def _terminate(self, timeout=1.0):
         """ Terminate the sub process.
@@ -280,8 +280,6 @@ class VideoReaderAbstract(object):
             self._terminate()
             err1 = str(err)
             raise RuntimeError("%s" % (err1,))
-        if arr.dtype in (np.uint16, np.dtype('>u2'), np.dtype('<u2')):
-            return arr.view(np.uint16)
         return arr
 
     def _readFrame(self):
@@ -355,6 +353,8 @@ class VideoWriterAbstract(object):
         none
 
         """
+        self._proc = None
+        self._cmd = None
         self.DEVNULL = open(os.devnull, 'wb')
 
         filename = os.path.abspath(filename)
@@ -478,7 +478,6 @@ class VideoWriterAbstract(object):
         if self._proc.stdin:
             self._proc.stdin.close()
         self._proc.wait()
-        self._proc = None
         self.DEVNULL.close()
 
     def writeFrame(self, im):